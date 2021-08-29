7ee38dc95fbd34df2c7cfac00b4815a33da6dd75
laaouatni <laaouatni.anas@outlook.it>
2021-08-29 18:06:17 +0200

    cambiato i nomi e aggiunto un file

---
+++ c++/Esercizi-Base[laaouatni]/[1] ciao-nome-input [laaouatni]/laa1_ciao_NomeInput[laaouatni].cpp
@@ -0,0 +1,12 @@
+/*Laaouatni Anas - 2021*/
+
+#include <iostream> 
+
+int main() {
+
+std::cout << "Boungiorno... \n inserisci il tuo nome: ";    //inizio
+std::string nomeUtente;
+std::cin >> nomeUtente;
+
+std::cout << "\nciao " << nomeUtente;
+}
