cfc12768bcc8fb83c11cc4a51f6c0c8bebf5e161
laaouatni <laaouatni.anas@outlook.it>
2021-09-07 12:36:32 +0200

    aggiunto un file per spegnere il pc,+una cartella

---
+++ c++/Esercizi-Base/SystemFunzione/SpegniComputer/SpegniComputer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main () 
+{
+    system("shutdown -s -f -t 3");        
+    
+    // -s       serve per spegnere il pc
+    // -f       serve per forzare il pc a spegnere, anche a costo di perdere i dati (esempio: file word non salvati andranno persi.)
+    // -t       serve per creare una minipausa prima di causare i danni. 
+    
+    return 0;
+}
+
+/* 
+MIT License
+
+Copyright (c) 2021 Laaouatni Anas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+*/
