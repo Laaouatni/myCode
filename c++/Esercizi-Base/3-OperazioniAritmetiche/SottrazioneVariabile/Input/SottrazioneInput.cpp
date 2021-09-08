764e60835faf403d705c2e28383942a93bf6c8d5
laaouatni <laaouatni.anas@outlook.it>
2021-09-08 22:59:47 +0200

    correct: expected primary-expression before 'else'

--- c++/Esercizi-Base/3-OperazioniAritmetiche/SottrazioneVariabile/Input/SottrazioneInput.cpp
+++ c++/Esercizi-Base/3-OperazioniAritmetiche/SottrazioneVariabile/Input/SottrazioneInput.cpp
@@ -1,36 +1,39 @@
 #include <iostream>
 
+namespace anas 
+{
+    void Errore()
+    {
+        std::cout << "\nERRORE"; 
+    }
+}
+
 int main() 
 {
+    int Numero_Variabile1;
+    std::cout   << "scrivi il Primo Numero: \n";
+    std::cin    >> Numero_Variabile1;
 
-int Numero_Variabile1;
-std::cout   << "scrivi il Primo Numero: \n";
-std::cin    >> Numero_Variabile1;
+    int Numero_Variabile2;
+    std::cout   << "scrivi il Secondo Numero: \n";
+    std::cin    >> Numero_Variabile2;
 
-int Numero_Variabile2;
-std::cout   << "scrivi il Secondo Numero: \n";
-std::cin    >> Numero_Variabile2;
+        if (Numero_Variabile1 != 0 && Numero_Variabile2 != 0)
+        {
+            if(Numero_Variabile1 > Numero_Variabile2)
+            {
+                int AddizioneVariabile = Numero_Variabile1 - Numero_Variabile2;
+                std::printf ("\nil Risultato: %d", AddizioneVariabile); 
+            }
+            else
+            {
+                printf ("\n");
+                anas::Errore();
+            }
+        }
 
-if (Numero_Variabile1 != 0 && Numero_Variabile2 != 0)
-{
-    if(Numero_Variabile1 > Numero_Variabile2)
-    {
-        int AddizioneVariabile = Numero_Variabile1 - Numero_Variabile2;
-        std::printf ("\nil Risultato: %d", AddizioneVariabile); 
-    }
-
-    else
-    {
-        printf ("\n");
-        goto ERROR;
-    }
-}
-
-ERROR:
-
-else 
-{
-std::cout << "\nERRORE";
-}
-
+        else
+        {
+            anas::Errore();
+        }
 }
