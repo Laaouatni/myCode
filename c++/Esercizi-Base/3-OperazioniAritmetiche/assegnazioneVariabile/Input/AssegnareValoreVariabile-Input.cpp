62565ef06f828baf13f49785bd697add419e38b5
laaouatni <laaouatni.anas@outlook.it>
2021-09-08 22:00:31 +0200

    Aggiunto OPERAZIONI-ARITMETICHE, spostato cartelle

---
+++ c++/Esercizi-Base/3-OperazioniAritmetiche/assegnazioneVariabile/Input/AssegnareValoreVariabile-Input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+
+int Numero_Variabile = 10;
+std::endl;
+std::cin >> Numero_Variabile;
+std::printf ("il numero della Variabile è: %d",Numero_Variabile);
+}
---
+++ c++/Esercizi-Base/3-OperazioniAritmetiche/assegnazioneVariabile/Normal/AssegnareValoreVariabile.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+
+int Numero_Variabile = 10;
+std::printf("il numero della Variabile è: %d",Numero_Variabile);
+}
