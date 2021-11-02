fb1b0fb1c229a28ab99b095a5bc8438814aa8108
laaouatni <laaouatni.anas@outlook.it>
2021-11-02 20:15:39 +0100

    console.log("hello world"); :)

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,8 +1,1 @@
-let hello = "hello world";
-console.log(hello + "\n");
-console.log(hello.length);
-
-for (let index = 0; index < hello.length; index++) {
-    const element = hello[index];
-    console.log(element);
-}
+console.log("hello world")
