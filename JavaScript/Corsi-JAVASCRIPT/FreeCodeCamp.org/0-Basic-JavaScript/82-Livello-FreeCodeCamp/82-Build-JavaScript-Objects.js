4ac61b74ccdc50c136b532f8af0631ee4ceebc48
laaouatni <laaouatni.anas@outlook.it>
2021-11-10 21:11:11 +0100

    JS Esercizio 0-82 FreeCodeCamp Corso

---
+++ JavaScript/Corsi-JAVASCRIPT/FreeCodeCamp.org/0-Basic-JavaScript/82-Livello-FreeCodeCamp/82-Build-JavaScript-Objects.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+
+    "name": "hello world",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["hello", "world"]
+
+    // Only change code above this line
+};
