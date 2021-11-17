2810623fc2e4348ba3cf6407a9f4cab30e672bcf
laaouatni <laaouatni.anas@outlook.it>
2021-11-17 20:55:55 +0100

    primo js in afp funzionante con alert

--- HTML-CSS/TEST/index.js
+++ HTML-CSS/TEST/index.js
@@ -1,10 +1,23 @@
 function radioCheck(radio) {
-    data = radio;
-    if (data == 1) {
+    /*     switch (radio) {
+            case "1":
+                alert("1");
+                break;
+            case "2":
+                alert("2");
+                break;
+            case "3":
+                alert("3");
+                break;
+            default:
+                alert("Invalid radio")
+                break;
+        } */
+    if (radio == "1") {
         alert("1");
-    } else if (data == 2) {
+    } else if (radio == "2") {
         alert("2");
-    } else if (data == 3) {
+    } else if (radio == "3") {
         alert("3");
     } else {
         alert("something go wrong");
