492b7645765da2634aed44eda95d6dbe926a8962
laaouatni <laaouatni.anas@outlook.it>
2021-11-17 21:07:48 +0100

    finalmente ho creato la funzione che volevo! js

--- HTML-CSS/TEST/index.js
+++ HTML-CSS/TEST/index.js
@@ -4,21 +4,21 @@ function radioCheck(radio) {
     const tre = document.getElementById("3-corsi-div");
 
     if (radio == "1") {
-        uno.classList.add("on");
-        due.classList.add("off");
-        tre.classList.add("off");
+        uno.className = 'corsi-div on';
+        due.className = 'corsi-div off';
+        tre.className = 'corsi-div off';
         console.log("1");
     } else
     if (radio == "2") {
-        uno.classList.add("off");
-        due.classList.add("on");
-        tre.classList.add("off");
+        uno.className = 'corsi-div off';
+        due.className = 'corsi-div on';
+        tre.className = 'corsi-div off';
         console.log("2");
     } else
     if (radio == "3") {
-        uno.classList.add("off");
-        due.classList.add("off");
-        tre.classList.add("on");
+        uno.className = 'corsi-div off';
+        due.className = 'corsi-div off';
+        tre.className = 'corsi-div on';
         console.log("3")
     } else {
         alert("something go wrong");
