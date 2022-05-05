cf87e42c0da3fad2957f01d10511805ecb136a70
laaouatni <laaouatni.anas@outlook.it>
2022-05-05 19:24:07 +0200

    REACT 0-2 FreeCodeCamp Corso

--- Front-End-Development-Libraries/FreeCodeCamp/React/1-Livello-FreeCodeCamp/1-Create-a-Simple-JSX-Element.jsx
+++ Front-End-Development-Libraries/FreeCodeCamp/React/1-Livello-FreeCodeCamp/1-Create-a-Simple-JSX-Element.jsx
@@ -1,1 +1,1 @@
-const JSX = <h1>hello JSX</h1>;
+const JSX = <h1>hello JSX!</h1>;
---
+++ Front-End-Development-Libraries/FreeCodeCamp/React/2-Livello-FreeCodeCamp/2-Create-a-Complex-JSX-Element.jsx
@@ -0,0 +1,11 @@
+const JSX =  
+<div>
+    <h1>Hello World</h1>
+    <p>Hello World</p>
+
+    <ul>
+        <li>Hello World</li>
+        <li>Hello World</li>
+        <li>Hello World</li>
+    </ul>
+</div>;
