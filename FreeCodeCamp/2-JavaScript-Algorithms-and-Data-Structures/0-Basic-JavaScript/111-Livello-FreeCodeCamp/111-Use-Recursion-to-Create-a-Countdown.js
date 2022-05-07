c7e2cd64e5aeae6733e619d4de7d274ffbf2e248
laaouatni <laaouatni.anas@outlook.it>
2022-05-07 19:24:46 +0200

    JS 0-111 FreeCodeCamp Corso

---
+++ FreeCodeCamp/2-JavaScript-Algorithms-and-Data-Structures/0-Basic-JavaScript/111-Livello-FreeCodeCamp/111-Use-Recursion-to-Create-a-Countdown.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+
+function countdown(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        const countArray = countdown(n - 1);
+        countArray.unshift(n);
+        return countArray;
+    }
+}
+console.log(countdown(5));
+
+// Only change code above this line
