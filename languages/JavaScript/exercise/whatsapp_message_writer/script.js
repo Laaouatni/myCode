c4e7d6be1aa9dff1ffe612fc4c139658ff0719f0
laaouatni <laaouatni.anas@outlook.it>
2022-05-28 19:25:01 +0200

    is worrking whtasapp message sender

--- languages/JavaScript/exercise/whatsapp_message_writer/script.js
+++ languages/JavaScript/exercise/whatsapp_message_writer/script.js
@@ -1,26 +1,27 @@
-let props = {
-    "text": "test message",
-    "num": 10,
-    "input": document.querySelectorAll("._13NKt")[1],
-    "button": document.querySelectorAll("._3HQNh")[1].querySelectorAll("button")[0],
-    "event": new InputEvent("input", { bubbles: true })
-}
-
 function spamMessage(props) {
     console.log("0")
-    window.InputEvent = window.Event || window.InputEvent;\
+    window.InputEvent = window.Event || window.InputEvent;
     console.log("1")
     props.event();
     console.log("2")
     props.input.innerHTML = props.text;
     console.log("3")
-    props.input.dispatchEvent(props.event);
+    props.input.dispatchEvent(props.event());
     console.log("4")
     props.button.click();
     console.log("5")
 }
 
+setInterval(() => {
+    let props = {
+        "text": "test message",
+        "num": 10,
+        "input": document.querySelectorAll("._13NKt")[1],
+        "button": document.querySelectorAll("._3HQNh")[1].querySelectorAll("button")[0],
+        "event": () => {
+            return new InputEvent("input", { bubbles: true });
+        }
+    }
 
-setInterval((props) => {
     spamMessage(props);
-}, 500)
+}, 500);
