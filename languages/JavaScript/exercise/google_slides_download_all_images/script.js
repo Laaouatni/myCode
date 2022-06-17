6ebee43167b96e3386fff858d9e44ca38db4923d
laaouatni <laaouatni.anas@outlook.it>
2022-06-17 16:40:05 +0200

    download all images from google slides using javascript

---
+++ languages/JavaScript/exercise/google_slides_download_all_images/script.js
@@ -0,0 +1,23 @@
+let arr = [];
+
+document.querySelectorAll("image")
+    .forEach((a) => {
+        let link = a.href.baseVal;
+        download(link);
+
+        arr.push(link);
+    });
+
+console.log(arr);
+
+function download(source) {
+    var el = document.createElement("a");
+
+    el.setAttribute("href", source);
+    el.setAttribute("download");
+
+    document.body.appendChild(el);
+
+    el.click();
+    el.remove();
+}
