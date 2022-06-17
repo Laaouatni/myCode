2a30f15ecd37a2bd019296abfe5ff0cb3ff7525e
laaouatni <laaouatni.anas@outlook.it>
2022-06-17 17:29:46 +0200

    mini-fixes on console.log in script.js of auto-image-downloader

--- languages/JavaScript/exercise/google_slides_automation/download_all_images/script.js
+++ languages/JavaScript/exercise/google_slides_automation/download_all_images/script.js
@@ -1,19 +1,23 @@
 let arr = [];
 
+let notDownNum = 0;
+
 document.querySelectorAll("image")
     .forEach((a, index) => {
         let link = a.href.baseVal;
+
         if (link != "//ssl.gstatic.com/docs/presentations/images/animation_indicator.svg") {
             downloadImage(link, index);
 
             arr.push(link);
             console.log(`✅ ${index + 1}/${arr.length} - ${link}`);
         } else {
-            console.log(`❌ ${index + 1}/${arr.length} - ${link}`);
+            notDownNum++;
+            console.log(`❌ ${index + 1}/${arr.length} - ${notDownNum} - ${link}`);
         }
     });
 
-console.log(`✅✅✅ finished downloading ${arr.length} images`, arr);
+console.log(`✅✅✅ finished downloading ${arr.length - notDownNum}/${arr.length} images`);
 
 async function downloadImage(imageSrc, index) {
     const image = await fetch(imageSrc);
