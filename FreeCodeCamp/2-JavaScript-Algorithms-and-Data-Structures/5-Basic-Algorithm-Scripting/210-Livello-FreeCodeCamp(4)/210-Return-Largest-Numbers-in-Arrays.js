99c3f9ee070dfd4daf9648a1525695bf9064698f
laaouatni <laaouatni.anas@outlook.it>
2023-01-06 22:40:37 +0100

    JS 5-210 FreeCodeCamp — Basic Algorithm Scripting — "Return Largest Numbers in Arrays"

---
+++ FreeCodeCamp/2-JavaScript-Algorithms-and-Data-Structures/5-Basic-Algorithm-Scripting/210-Livello-FreeCodeCamp(4)/210-Return-Largest-Numbers-in-Arrays.js
@@ -0,0 +1,12 @@
+function largestOfFour(arr) {
+  return arr.map((item) => {
+    return Math.max(...item);
+  });
+}
+
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
